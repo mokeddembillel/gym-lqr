a617de08b747df3b6c12eaa507545222f05f210b
billel mokeddem <bilel.crysis3@gmail.com>
2021-02-11 19:39:48 +0100

    Adding env parameters to the register function

--- gym-lqr/__init__.py
+++ gym-lqr/__init__.py
@@ -3,4 +3,6 @@ from gym.envs.registration import register
 register(
     id='lqr-v0',
     entry_point='gym_lqr.envs:FooEnv',
+    max_episode_steps=150,
+    kwargs={'dim' : 1, 'init_x' : 10., 'x_bound' : np.inf},
 )
