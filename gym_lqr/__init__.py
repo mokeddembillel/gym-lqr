746027755d014816e643984d4e85d0acb6ba61ba
billel mokeddem <bilel.crysis3@gmail.com>
2021-02-21 04:41:15 +0100

    Integrate the new environment in the gym_lqr package

--- gym_lqr/__init__.py
+++ gym_lqr/__init__.py
@@ -7,3 +7,10 @@ register(
     max_episode_steps=1000,
     kwargs={'dim' : 1, 'x_bound' : np.inf, 'u_bound' : 5},
 )
+
+register(
+    id='lqr-stochastic-v0',
+    entry_point='gym_lqr.envs:LqrEnvStochastic',
+    max_episode_steps=1000,
+    kwargs={'dim_state' : 3, 'dim_action': 3, 'x_bound' : np.inf, 'u_bound' : 5},
+)
--- gym_lqr/envs/__init__.py
+++ gym_lqr/envs/__init__.py
@@ -1,1 +1,2 @@
 from gym_lqr.envs.lqr_env import LqrEnv
+from gym_lqr.envs.lqr_env_stochastic import LqrEnvStochastic
