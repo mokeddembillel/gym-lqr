518f444eefbccc786b4686731e804f81c43821b1
Billel Mokeddem <billel.mokeddem.ml@gmail.com>
2021-06-25 15:21:51 +0100

    Adding the code to compute the analytical Q value

--- gym_lqr/envs/lqr_env.py
+++ gym_lqr/envs/lqr_env.py
@@ -24,6 +24,10 @@ class LqrEnv(gym.Env):
         self.S = None
         self.B = None
         self.R = None
+
+        self.gamma = 0.9
+        self.P = np.ones((self.n, self.n))
+
         
         # Set State space
         self.observation_space = spaces.Box(low=-x_bound, high=x_bound, shape=(self.n,))
@@ -45,16 +49,37 @@ class LqrEnv(gym.Env):
     #     B = np.dot(A, A.transpose())
     #     B[i][i] += 1 for i in range(len(B))
     #     print B
-        
+    
+    def get_Q(self, x, u):
+        q = self.get_cost(x, u) + self.gamma * self.get_V(x)
+        return -q
+    
+    def get_V(self, x):
+        v = np.dot(np.dot(np.squeeze(x.transpose()), self.P), np.squeeze(x))
+        return v
+
+    def get_cost(self, x, u):
+        # print(x)
+        # print(u)
+        c = np.dot(np.dot(np.squeeze(x.transpose()), self.S), np.squeeze(x)) + \
+            np.dot(np.dot(np.squeeze(u.transpose()), self.R), np.squeeze(u))
+        return c
+    
+    def get_P(self):
+        return self.P
+    
+    def set_P(self, P):
+        self.P = P
+
      
     def reset(self, init_x, max_steps):
-        self.x = np.array([init_x])
+        self.x = init_x
 
         self.S = np.identity(self.n)
         self.R = np.identity(self.m)
 
         self.A = np.ones((self.n,self.n))
-        self.B = np.ones((self.m,self.m))
+        self.B = np.ones((self.n,self.m))
 
         self.steps = 0
         self.max_steps = max_steps
@@ -66,11 +91,19 @@ class LqrEnv(gym.Env):
         self.x_old = self.x
         
         # Calculating the cost
-        self.c = np.dot(np.dot(np.squeeze(self.x.transpose()), self.S), np.squeeze(self.x)) + \
-            np.dot(np.dot(np.squeeze(self.u.transpose()), self.R), np.squeeze(self.u))
-        
+        self.c = self.get_cost(self.x, self.u)
+        # print(self.c)
         # Calculating the new State
         self.x = np.dot(self.A, np.squeeze(self.x)) + np.dot(self.B, np.squeeze(self.u))
+        # print(self.x)
+
+        self.P = self.S \
+            + self.gamma * np.dot(np.dot(np.squeeze(self.A.transpose()), self.P), self.A) \
+            - np.power(self.gamma, 2) * np.dot(np.dot(np.dot(np.dot(np.dot(np.dot(np.squeeze(self.A.transpose()), self.P), self.B), \
+            np.linalg.inv(self.R + self.gamma * np.dot(np.dot(np.squeeze(self.B.transpose()), self.P), self.B))), \
+            np.squeeze(self.B.transpose())), self.P), self.A)
+
+        #print(self.P)
 
         self.steps += 1
         return self.x, -self.c, 1 if self.steps > self.max_steps else 0, None
